0ece94e1a3d0dc5bf68c043fb5374ecb9f4e73d3
sirkaue <kauekdslopes@gmail.com>
2024-06-28 18:35:41 -0300

    first commit

---
+++ src/test/java/com/sirkaue/screensound/ScreensoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirkaue.screensound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreensoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
